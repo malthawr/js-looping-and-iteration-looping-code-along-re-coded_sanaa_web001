1fcd34cc1429dad2cef960faa62d41c09c00d07c
Mohammed Al-Thawr <malthawr@gmail.com>
2020-09-09 06:11:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const birthday=  [ 'Lisa', 'Kaitlin', 'Jan' ];
+
+function writeCards(birthday) {
+  for (let i = 0; i <birthday.length; i++) {
+    console.log(`Thank you, ${birthday[i]} , for the wonderful birthday gift!`);
+  }
+ 
+ return birthday;
+}
+
+let countdown = 10;
+while (countdown > 0) {
+  console.log(countdown--);
+}
+
