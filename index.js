5a5691e8e674442fd2c87da831aeeda716f08f5a
Mohammed Al-Thawr <malthawr@gmail.com>
2020-09-09 06:13:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 // Code your solutions in this file
-const birthday=  [ 'Lisa', 'Kaitlin', 'Jan' ];
-
-function writeCards(birthday) {
-  for (let i = 0; i <birthday.length; i++) {
-    console.log(`Thank you, ${birthday[i]} , for the wonderful birthday gift!`);
+function writeCards( namesArray, event ) {
+  let thankYouCards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
   }
- 
- return birthday;
+  return thankYouCards
 }
 
-let countdown = 10;
-while (countdown > 0) {
-  console.log(countdown--);
+function countDown( startingNumber ) {
+  while ( startingNumber > 0 ) {
+    console.log( startingNumber );
+    startingNumber -= 1;
+  }
+  console.log( startingNumber );
 }
-
